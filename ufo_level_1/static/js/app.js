a8770a2edba87207cd6900562f66d68776c071fe
Kapil Pundhir <70111855+kman4@users.noreply.github.com>
2021-01-14 22:24:18 -0500

    commit

---
+++ ufo_level_1/static/js/app.js
@@ -0,0 +1,38 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+// Clearing Data
+var tbody = d3.select("tbody");
+
+
+// Creating Row and cell
+
+function buildData(data) {
+    tbody.html("");
+data.forEach((datarow) => {
+    var row = tbody.append("tr");
+    Object.values(datarow).forEach((value) =>{
+    var cell = row.append("td");
+    cell.text(value);
+    });
+    
+});
+
+}
+
+// Creating function to retrieve data on click
+function handleclick() {
+    var date = d3.select("#datetime").property("value");
+    var filterdata = tableData;
+    if(date) {
+        filterdata = filterdata.filter(row => row.datetime === date);
+    }
+    buildData(filterdata);
+
+}
+// On click function
+d3.selectAll("#filter-btn").on("click",handleclick);
+
+buildData(tableData);
+
